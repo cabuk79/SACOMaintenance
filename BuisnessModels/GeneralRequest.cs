7843d7c6df2b4f091cb3d68dd4a0333359788a7e
cabuk79 <cabuk79@googlemail.com>
2021-02-01 20:32:29 +0000

    Added daily register model and updated the general and plant requests with properties

--- BuisnessModels/GeneralRequest.cs
+++ BuisnessModels/GeneralRequest.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace BuisnessModels
 {
-    class GeneralRequest
+    public class GeneralRequest
     {
+        public int Id { get; set; }
+        public int MaintRequestId { get; set; } //FK to MaintRequestInitiation Id
+        public int AuthorityToWorkUserId { get; set; }
+        public DateTime AuthorityToWorkDateTime { get; set; }
+        public string DetailOfWorkCompleted { get; set; }
+        public int DetailOfWorkCompeltedUserId { get; set; }
+        public DateTime DetailOfWorkCompeltedDateTime { get; set; }
+        public int WorkCompletedYesNo { get; set; }
+        public string WorkCompletedDetails { get; set; }
+        public string SignOffDetails { get; set; }
+        public int WorkCompletedSignOffYesNo { get; set; }
+        public int WorkCompletedSignOffUserId { get; set; }
+        public int WorkCompletedSignOffDateTime { get; set; }
     }
 }
--- BuisnessModels/PlantRequest.cs
+++ BuisnessModels/PlantRequest.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace BuisnessModels
 {
-    class PlantRequest
+    public class PlantRequest
     {
+        public int IsolationNitricAcid { get; set; }
+        public int IsolatedPhosphoricAcid { get; set; }
+        public int IsolatedSodiumHydroxide { get; set; }
+        public int IsolatedSulphuricAcid { get; set; }
+        public string IsolatedOther { get; set; }
+
+
+        public int DrainingLinesNitricAcid { get; set; }
+        public int DrainingLinesPhosphoricAcid { get; set; }
+        public int DrainingLinesSodiumHydroxide { get; set; }
+        public int DrainingLinesSulphuricAcid { get; set; }
+        public string DrainingLinesOther { get; set; }
+
+        public int IsolationsCompressedAir { get; set; }
+        public int IsolationsElectrical { get; set; }
+        public int IsolationsGas { get; set; }
+        public int IsolationsMechanical { get; set; }
+        public int IsolationsSteam { get; set; }
+        public int IsolationsWater { get; set; }
+        public string IsolationsOther { get; set; }
+
+        public string OtherPrecautions { get; set; }
+
+        public int AuthorisationToWorkUserId { get; set; }
+        public DateTime AuthorisationDatTime { get; set; }
+
+        public int ReceiptPersonDoingWorkUserId { get; set; }
+        public int ReceiptCompanyId { get; set; }
+        public string ReceiptCompany { get; set; }
+        public DateTime ReceiptDateTime { get; set; }
+
+
+        public string ClearanceDetails { get; set; }
+        public int ClearanceCompanyId { get; set; }
+        public string ClearancePosition { get; set; }
+        public DateTime ClearanceDateTime { get; set; }
+
+        public List<RequestDailyRegister> ListOfDailyRgisters = new List<RequestDailyRegister>(); //List of all the daily registers linked to the request
     }
 }
+
+
+
+
+
+
+
+
+
+
