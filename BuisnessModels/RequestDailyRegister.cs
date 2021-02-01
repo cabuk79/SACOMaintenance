b53f1ecb068699b8eff6cb47dfd3324c5b5fcf3a
cabuk79 <cabuk79@googlemail.com>
2021-02-01 20:39:24 +0000

    Added RequestDailyRegister model and properties

---
+++ BuisnessModels/RequestDailyRegister.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuisnessModels
+{
+    public class RequestDailyRegister
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime TimeIn { get; set; }
+        public DateTime TimOut { get; set; }
+        public int MaintReqiestId { get; set; } //FK to the MaintRequestInitiation ID
+    }
+}
